30fab0030304d082de40be17a1edf1b7cbc48acd
Matheus Warkentin <matheus.warkentin@brainz.group>
2022-03-02 09:24:50 -0300

    heroku adjustments

--- server.js
+++ server.js
@@ -12,18 +12,18 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
 
         app.use(bodyParser.json())
         app.use(bodyParser.urlencoded({ extended: true }))
-        app.use(function(req, res, next) {
+        app.use(function (req, res, next) {
             res.header('Access-Control-Allow-Origin', '*');
             res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
             res.header(
-              'Access-Control-Allow-Headers',
-              'Origin, X-Requested-With, Content-Type, Accept'
+                'Access-Control-Allow-Headers',
+                'Origin, X-Requested-With, Content-Type, Accept'
             );
             next();
-          });
+        });
 
         app.get('/meds', (req, res) => {
-            medsCollection.find({client:req.query.id}).toArray()
+            medsCollection.find({ client: req.query.id }).toArray()
                 .then(results => {
                     res.send(results);
                 })
@@ -48,8 +48,12 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
             )
                 .catch(error => console.error(error))
         })
-        app.listen(3000, function () {
-            console.log('listening on 3000')
+        let port = process.env.PORT;
+        if (port == null || port == "") {
+            port = 3000;
+        }
+        app.listen(port, function () {
+            console.log('listening on ' + port)
         })
     })
     .catch(error => console.error(error))
